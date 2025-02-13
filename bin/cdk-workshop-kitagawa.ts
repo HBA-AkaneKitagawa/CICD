ec55716aae1d3fedec8789cb65357f616ea77106
EC2 Default User <ec2-user@ip-172-31-27-98.ap-northeast-1.compute.internal>
2025-02-13 09:14:58 +0000

    first commit

--- bin/cdk-workshop-kitagawa.ts
+++ bin/cdk-workshop-kitagawa.ts
@@ -3,4 +3,9 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkWorkshopKitagawaStack } from '../lib/cdk-workshop-kitagawa-stack';
 
 const app = new cdk.App();
-new CdkWorkshopKitagawaStack(app, 'CdkWorkshopKitagawaStack');
+new CdkWorkshopKitagawaStack(app, 'CdkWorkshopKitagawaStack',{
+    env:{
+        account: "701394445941",
+        region:"ap-northeast-3"
+    }
+});
--- lib/cdk-workshop-kitagawa-stack.ts
+++ lib/cdk-workshop-kitagawa-stack.ts
@@ -1,19 +1,71 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+// ec2 に関するパッケージを import
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+// ファイルを読み込むためのパッケージを import
+import { readFileSync } from "fs";
+// CfnOutput を import
+import { CfnOutput } from "aws-cdk-lib";
+// rds に関するパッケージを import
+import * as rds from "aws-cdk-lib/aws-rds";
+// ALB に関するパッケージを import
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+// target を追加するためのパッケージimport
+import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 
 export class CdkWorkshopKitagawaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopKitagawaQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // vpc を宣言
+    const vpc = new ec2.Vpc(this, "BlogVpcKitagawa", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopKitagawaTopic');
+    // EC2 インスタンスの宣言を準備
+    const webServer100 = new ec2.Instance(this, "WordpressServer100", {
+      vpc,
+      // t3.small インスタンスタイプを指定
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.SMALL,
+      ),
+      // AmazonLinuxImage インスタンスを生成し、AMI を設定
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+      // EC2 インスタンスを配置するサブネットを指定
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+    });
+
+    // user-data.sh を読み込み、変数に格納
+    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
+    // EC2 インスタンスにユーザーデータを追加
+    webServer100.addUserData(script);
+
+    // port80, 全ての IP アドレスからのアクセスを許可
+    webServer100.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+
+    // EC2 インスタンスアクセス用の IP アドレスを出力
+    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
+      value: `http://${webServer100.instancePublicIp}`,
+    });
+
+    // RDS のインスタンスを宣言
+    const dbServer = new rds.DatabaseInstance(this, "WordPressDB100", {
+      vpc,
+      // DatabaseInstanceEngine クラスを利用してデータベースエンジンを設定
+      engine: rds.DatabaseInstanceEngine.mysql({version: rds.MysqlEngineVersion.VER_8_0_36,}),
+      // RDS DB インスタンスのインスタンスタイプを設定
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.SMALL
+        ),
+      // RDS DB インスタンスのデータベース名を設定
+      databaseName: "wordpress",
+    });
+    // WebServer からのアクセスを許可
+    dbServer.connections.allowDefaultPortFrom(webServer100);
   }
 }
